f6ecc8826094489f2ef7deab19e1c224aa3d2c8c
sharath <sharath.r@ge.com>
2018-02-14 13:23:44 +0530

    - swagger/actuator working

--- src/main/java/com/ip/SwaggerConfig.java
+++ src/main/java/com/ip/SwaggerConfig.java
@@ -11,6 +11,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -21,15 +22,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ip.*"))
-                .paths(regex("/*"))
-                .build()
-                .apiInfo(metaData());
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
+
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.ip.*"))
+//                .paths(regex("/*"))
+//                .build()
+//                .apiInfo(metaData());
+//    }
     private ApiInfo metaData() {
         ApiInfo apiInfo = new ApiInfo(
                 "Itest API",
--- src/main/java/com/ip/SystemArchitecture.java
+++ src/main/java/com/ip/SystemArchitecture.java
@@ -1,30 +1,30 @@
-///**
-// * Copyright (C) General Electric Company 2018 . All Rights Reserved.
-// * 
-// * @author 999951
-// *
-// */
-//package com.ip;
-//
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.stereotype.Component;
-//
-//@Aspect
-//@Component
-//public class SystemArchitecture {
-//	// Point cut for all productionscheduling api
-//	@Pointcut("execution(* com.ip.itest..*.*(..))")
-//	public void methodLoggerAop() {
-//	}
-//
-//	// Point cut for all api
-//	@Pointcut("execution(* com.ip.itest..*.*(..))")
-//	public void isControllerLayer() {
-//	}
-//
-//	// Point cut for service layer api
-//	@Pointcut("execution(* com.ip.itest..*.*(..))")
-//	public void inServiceLayer() {
-//	}
-//}
+/**
+ * Copyright (C) General Electric Company 2018 . All Rights Reserved.
+ * 
+ * @author 999951
+ *
+ */
+package com.ip;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SystemArchitecture {
+	// Point cut for all productionscheduling api
+	@Pointcut("execution(* com.ip.itest..*.*(..))")
+	public void methodLoggerAop() {
+	}
+
+	// Point cut for all api
+	@Pointcut("execution(* com.ip.itest..*.*(..))")
+	public void isControllerLayer() {
+	}
+
+	// Point cut for service layer api
+	@Pointcut("execution(* com.ip.itest..*.*(..))")
+	public void inServiceLayer() {
+	}
+}
